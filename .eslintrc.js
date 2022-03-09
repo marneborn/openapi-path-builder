0c183c822a325bb5c045feeaa69e6c4153538734
Mikael Arneborn <marneborn@gmail.com>
2022-03-09 13:52:06 -0800

    stuff

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,34 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  ignorePatterns: [
+    '!.eslintrc.js',
+  ],
+  overrides: [
+    {
+      env: {
+        jest: true,
+      },
+      files: ['src/**/*.spec.ts'],
+      rules: {
+        'func-names': 'off',
+        'import/no-extraneous-dependencies': 'off',
+      },
     },
-    "extends": [
-        "airbnb-base"
-    ],
-    ignorePatterns: [
-      '!.eslintrc.js',
-    ],  
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "@typescript-eslint"
-    ],
-    "rules": {
-    }
-}
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  rules: {
+  },
+};
--- src/serializePath/serializePath.spec.ts
+++ src/serializePath/serializePath.spec.ts
@@ -1,8 +1,8 @@
 describe('serializePath', () => {
   it('should pass', () => {
     expect(true).toBe(true);
-  })
+  });
   it('should fail', () => {
     expect(true).toBe(false);
-  })
-})
+  });
+});
