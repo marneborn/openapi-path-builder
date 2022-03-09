a08e165f4f177286401eb24a16d376f8057f89e8
Mikael Arneborn <marneborn@gmail.com>
2022-03-09 13:57:25 -0800

    eslint .ts and .js different

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,11 +5,35 @@ module.exports = {
   },
   extends: [
     'airbnb-base',
+    'plugin:json/recommended',
   ],
   ignorePatterns: [
     '!.eslintrc.js',
   ],
   overrides: [
+    {
+      extends: [
+        'plugin:@typescript-eslint/recommended',
+      ],
+      files: ['**/*.ts', '**/*.tsx'],
+      parser: '@typescript-eslint/parser',
+      rules: {
+        '@typescript-eslint/explicit-function-return-type': ['error', {
+          allowExpressions: true,
+        }],
+        '@typescript-eslint/no-explicit-any': 'error',
+        '@typescript-eslint/no-shadow': 'error',
+        '@typescript-eslint/no-unused-vars': 'error',
+        '@typescript-eslint/semi': ['error'],
+        'no-shadow': 'off',
+        semi: 'off',
+      },
+      settings: {
+        'import/resolver': {
+          typescript: {},
+        },
+      },
+    },
     {
       env: {
         jest: true,
@@ -28,7 +52,34 @@ module.exports = {
   },
   plugins: [
     '@typescript-eslint',
+    'sort-keys-fix',
   ],
   rules: {
+    'class-methods-use-this': 'off',
+    'comma-dangle': 'error',
+    'import/extensions': 'off',
+    indent: ['error', 2, {
+      VariableDeclarator: 2,
+    }],
+    'max-len': ['error', {
+      code: 150,
+      ignoreComments: true,
+      ignoreRegExpLiterals: true,
+      ignoreStrings: true,
+      ignoreTemplateLiterals: true,
+      ignoreTrailingComments: false,
+      ignoreUrls: true,
+      tabWidth: 2,
+    }],
+    'multiline-ternary': ['error', 'always-multiline'],
+    'no-console': 'error',
+    'no-nested-ternary': 'warn',
+    'operator-linebreak': ['error', 'before', {
+      overrides: {
+        '=': 'none',
+      },
+    }],
+    semi: ['error', 'always'],
+    'sort-keys-fix/sort-keys-fix': 'error',
   },
 };
