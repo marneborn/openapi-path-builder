fd883b5a5b89359bd1ea093cca7aad22fcad8776
Mikael Arneborn <marneborn@gmail.com>
2022-03-09 15:17:57 -0800

    started getBasePath

---
+++ src/getBasePath/getBasePath.spec.ts
@@ -0,0 +1,13 @@
+import getBasePath from './index';
+
+describe('getBasePath', () => {
+  it('should return "" if there is no server', () => {})
+
+  it('should return "/api" if the path in the first server is /api', () => {})
+  
+  it('should strip off any trailing slashes', () => {})
+  
+  it('should use the basePath in the first server', () => {})
+  
+  it('should return "" if there is a / at the end of the server', () => {})
+})
---
+++ src/getBasePath/index.ts
@@ -0,0 +1,1 @@
+const getBasePath = (): string => '';
