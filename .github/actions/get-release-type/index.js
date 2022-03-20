3474b90ddc98de0e8ccae10d8d8ba79872d3991b
Mikael Arneborn <marneborn@gmail.com>
2022-03-20 10:38:58 -0700

    release type force for testing

--- .github/actions/get-release-type/index.js
+++ .github/actions/get-release-type/index.js
@@ -8,18 +8,21 @@ const labelToVersion = {
 };
 
 try {
-  const labelNames = github.context.payload.pull_request.labels
-    .map((label) => label.name)
-    .filter((labelName) => labelToVersion[labelName]);
+  core.debug('Forced type for testing:');
+  core.setOutput('releaseType', labelToVersion('release-minor'));
 
-  if (labelNames.length === 0) {
-    core.setFailed('No labels set');
-  } else if (labelNames.length > 1) {
-    core.setFailed(`Multiple labels set: ${labelNames.join(', ')}`);
-  } else {
-    core.debug('release-type:', labelToVersion[labelNames[0]]);
-    core.setOutput('releaseType', labelToVersion[labelNames[0]]);
-  }
+  // const labelNames = github.context.payload.pull_request.labels
+  //   .map((label) => label.name)
+  //   .filter((labelName) => labelToVersion[labelName]);
+
+  // if (labelNames.length === 0) {
+  //   core.setFailed('No labels set');
+  // } else if (labelNames.length > 1) {
+  //   core.setFailed(`Multiple labels set: ${labelNames.join(', ')}`);
+  // } else {
+  //   core.debug('release-type:', labelToVersion[labelNames[0]]);
+  //   core.setOutput('releaseType', labelToVersion[labelNames[0]]);
+  // }
 } catch (error) {
   core.setFailed(error.message);
 }
