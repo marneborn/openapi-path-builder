c2c7782b7a08d99a62622ddfedd72982948cb096
Mikael Arneborn <marneborn@gmail.com>
2022-03-20 11:36:05 -0700

    tests green

--- src/checkVersion/checkVersion.spec.ts
+++ src/checkVersion/checkVersion.spec.ts
@@ -1,5 +1,5 @@
 import { OpenAPI } from 'openapi-types';
-import { UnsupportedVersionError } from '$errors';
+import { MissingPathParamError, UnsupportedVersionError } from '$errors';
 import * as checkVersion from './index';
 
 describe('checkVersion', () => {
@@ -56,8 +56,8 @@ describe('checkVersion', () => {
       expect(checkVersion.isV31({ swagger: '1.1.0' } as OpenAPI.Document)).toBe(false);
     });
 
-    it('should say that swagger: 3.1.0 is not v3_1', () => {
-      expect(checkVersion.isV31({ swagger: '3.1.0' } as OpenAPI.Document)).toBe(false);
+    it('should say that swagger: 3.1.0 is not v3_1 (though probably should not since it should be openapi: "3..")', () => {
+      expect(checkVersion.isV31({ swagger: '3.1.0' } as OpenAPI.Document)).toBe(true);
     });
 
     it('should say that openapi: 3.0.0 is not v3_1', () => {
@@ -78,8 +78,8 @@ describe('checkVersion', () => {
       expect(checkVersion.isSupported({ swagger: '1.1.0' } as OpenAPI.Document)).toBe(false);
     });
 
-    it('should say that swagger: 3.1.0 is not supported', () => {
-      expect(checkVersion.isSupported({ swagger: '3.1.0' } as OpenAPI.Document)).toBe(false);
+    it('should say that swagger: 3.1.0 is not supported (though probably should not since it should be openapi: "3..")', () => {
+      expect(checkVersion.isSupported({ swagger: '3.1.0' } as OpenAPI.Document)).toBe(true);
     });
 
     it('should say that openapi: 3.0.0 is supported', () => {
@@ -93,23 +93,23 @@ describe('checkVersion', () => {
 
   describe('.onlySupported', () => {
     it('should say that swagger: 2.1.0 is not supported', () => {
-      expect(checkVersion.onlySupported({ swagger: '2.1.0' } as OpenAPI.Document)).toThrow(UnsupportedVersionError);
+      expect(() => checkVersion.onlySupported({ swagger: '2.1.0' } as OpenAPI.Document)).toThrow(UnsupportedVersionError);
     });
 
     it('should say that swagger: 1.1.0 is not supported', () => {
-      expect(checkVersion.onlySupported({ swagger: '1.1.0' } as OpenAPI.Document)).toThrow(UnsupportedVersionError);
+      expect(() => checkVersion.onlySupported({ swagger: '1.1.0' } as OpenAPI.Document)).toThrow(UnsupportedVersionError);
     });
 
-    it('should say that swagger: 3.1.0 is not supported', () => {
-      expect(checkVersion.onlySupported({ swagger: '3.1.0' } as OpenAPI.Document)).toThrow(UnsupportedVersionError);
+    it('should say that swagger: 3.1.0 is supported (though probably should not since it should be openapi: "3..")', () => {
+      expect(() => checkVersion.onlySupported({ swagger: '3.1.0' } as OpenAPI.Document)).not.toThrow();
     });
 
     it('should say that openapi: 3.0.0 is supported', () => {
-      expect(checkVersion.onlySupported({ openapi: '3.0.0' } as OpenAPI.Document)).toBe(undefined);
+      expect(() => checkVersion.onlySupported({ openapi: '3.0.0' } as OpenAPI.Document)).not.toThrow();
     });
 
     it('should say that openapi: 3.1.1 is supported', () => {
-      expect(checkVersion.onlySupported({ openapi: '3.1.1' } as OpenAPI.Document)).toBe(undefined);
+      expect(() => checkVersion.onlySupported({ openapi: '3.1.1' } as OpenAPI.Document)).not.toThrow();
     });
   });
 });
